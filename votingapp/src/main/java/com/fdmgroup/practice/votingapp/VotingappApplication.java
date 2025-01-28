cffc9bc76f34b1a016bf190d924f23934a77e032
conorreid97 <conorreid97@gmail.com>
2025-01-28 12:16:46 +0000

    Added voting app and set up database

---
+++ votingapp/src/main/java/com/fdmgroup/practice/votingapp/VotingappApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.practice.votingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingappApplication.class, args);
+	}
+
+}
---
+++ votingapp/src/test/java/com/fdmgroup/practice/votingapp/VotingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.practice.votingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
